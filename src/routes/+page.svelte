98ac395b06f23772ef15050e7749b507c4c474d8
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2023-10-15 14:11:35 -0400

    Set up game
    Jankiest code I've ever written

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Player from "./Player.svelte";
+    import { onMount } from "svelte";
+
+    const gameWidth = 960, gameHeight = 540;
+
+    let playerObject;
+
+    function tick() {
+        playerObject.tick();
+    }
+
+    onMount(function() {
+        setInterval(tick, 1000 / 60);
+    });
+</script>
+
+<svg width={gameWidth} height={gameHeight} style="background-color: deepskyblue;">
+    <Player bind:this={playerObject}></Player>
+</svg>
---
+++ src/routes/Player.svelte
@@ -0,0 +1,32 @@
+<script context="module">
+    let current;
+</script>
+
+<script>
+    export let x = 0;
+    export let y = 0;
+    export let velocityX = 0;
+    export let velocityY = 0;
+    export let width = 50;
+    export let height = 50;
+
+    let keysDown = {};
+
+    window.addEventListener("keydown", function(e) {
+        keysDown[e.key] = true;
+    });
+    window.addEventListener("keyup", function(e) {
+        keysDown[e.key] = false;
+    });
+
+    export let tick = () => {
+        if (keysDown["ArrowRight"]) x++;
+        if (keysDown["ArrowLeft"]) x--;
+        if (keysDown["ArrowDown"]) y++;
+        if (keysDown["ArrowUp"]) y--;
+    };
+</script>
+
+<rect width={width} height={height} x={x} y={y} style="fill: navyblue;" rx={10} ry={10} />
+
+<svelte:options accessors={true} />
